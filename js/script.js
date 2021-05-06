ab6065221e1308345520393cd039fe8cecaed3e2
RyanShaw21 <>
2021-05-06 16:31:05 +0000

    Changed js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 // Created on: May 2021
 // This file contains the JS functions for index.html
 
+"use strict"
+
 function calculateClicked () {
   // this function calculates the Area and Perimeter of a rectangle
 
@@ -11,7 +13,11 @@ function calculateClicked () {
   const length = parseInt(document.getElementById("length").value)
   const width = parseInt(document.getElementById("width").value)
 
+  // process
+  const area = length * width
+  const perimeter = 2 * (length + width)
+
   // output
-  document.getElementById("area").innerHTML = 'The area is: ' + length + ' x ' + width + ' = ' + (length * width) + ' or ' + (length * width) + 'cm²'
-  document.getElementById("perimeter").innerHTML = 'The perimeter is: 2 x (' + length + ' + ' + width + ') = ' + (2 * (length + width)) + ' or ' + (2 * (length + width)) + 'cm'
+  document.getElementById("area").innerHTML = 'The area is: ' + length + ' x ' + width + ' = ' + area + ' or ' + area + ' cm²'
+  document.getElementById("perimeter").innerHTML = 'The perimeter is: 2 x (' + length + ' + ' + width + ') = ' + perimeter + ' or ' + perimeter + ' cm'
 }
